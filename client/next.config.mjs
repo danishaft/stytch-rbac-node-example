162e2fff2643fb60ef597d7d11e84bed9c798bfe
Danishaft <danielejeh2019@gmail.com>
2024-07-13 13:02:50 +0100

    chore[client][config]: add image and redirect config changes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'plus.unsplash.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'dbmgdlfeqxtzhnuazibe.supabase.co',
+          },
+          {
+            protocol: 'https',
+            hostname: 'images.unsplash.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'loremflickr.com',
+          },
+        ],
+      },
+      async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/workspace/inbox',
+            permanent: true,
+          },
+          {
+            source: '/workspace',
+            destination: '/workspace/inbox',
+            permanent: true,
+          },
+        ]
+      },
+};
 
 export default nextConfig;
