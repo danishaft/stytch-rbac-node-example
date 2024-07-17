268fcde1daec1a3bbb173de32b01d22f5867c5da
Danishaft <danielejeh2019@gmail.com>
2024-07-17 10:19:43 +0100

    deleted

--- client/app/workspace/departments/tasks/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-// list all the project tasks in the dept while specifying the project each task belongs to
-
-export const DepartmentTasks = () => {
-  return (
-    <div>DepartmentProject</div>
-  )
-}
