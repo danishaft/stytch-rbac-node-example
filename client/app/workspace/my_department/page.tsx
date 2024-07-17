d91106b8958097c7271653d3e4cdb10726f55cd6
Danishaft <danielejeh2019@gmail.com>
2024-07-17 10:22:08 +0100

    feat[client][comps]: add department specific pages

---
+++ client/app/workspace/my_department/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyDepartmentDetails() {
+  return (
+    <div>all dept details .all projects. or all dept project where user is a member</div>
+  )
+}
---
+++ client/app/workspace/my_department/tasks/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyDepartmentTasks() {
+  return (
+    <div>all dept tasks .all projects. or all personal tasks</div>
+  )
+}
