d0b7e6f9036c87a5530757d8e2e4b3ef62274c40
Danishaft <danielejeh2019@gmail.com>
2024-07-17 10:08:20 +0100

    feat[client][core]: add data for department and public projects

---
+++ client/app/utils/data/departmentData.ts
@@ -0,0 +1,172 @@
+import { DepartmentDataType } from "../types/Types";
+
+export const departmentData: DepartmentDataType[] = [
+    {
+        departmentId: '1',
+        identifier: 'ENG',
+        name: 'Engineering',
+        description: 'Lörem ipsum salig nen, ip-tv plus labesa. Eurov yk. Funktionell dumhet sabel som antiras mide. Heteron bionebelt preseling, divis peng. Trer beroren. ',
+        projects: [
+            {
+                projectId: '1',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+            {
+                projectId: '3',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+            {
+                projectId: '4',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+            {
+                projectId: '6',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+        ]
+    },
+    {
+        departmentId: '3',
+        identifier: 'ACC',
+        name: 'Accounting',
+        description: 'Lörem ipsum salig nen, ip-tv plus labesa. Eurov yk. Funktionell dumhet sabel som antiras mide. Heteron bionebelt preseling, divis peng. Trer beroren. ',
+        projects: [
+            {
+                projectId: '1',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+            {
+                projectId: '3',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+            {
+                projectId: '4',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+            {
+                projectId: '6',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+        ]
+    },
+    {
+        departmentId: '5',
+        identifier: 'MKT',
+        name: 'Marketing',
+        description: 'Lörem ipsum salig nen, ip-tv plus labesa. Eurov yk. Funktionell dumhet sabel som antiras mide. Heteron bionebelt preseling, divis peng. Trer beroren. ',
+        projects: [
+            {
+                projectId: '1',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+            {
+                projectId: '3',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+            {
+                projectId: '4',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+            {
+                projectId: '6',
+                name: "Project code",
+                lead: "Daniel Ejeh",
+                description: "This is a project description for Project code. This is a project description for Project Alpha.",
+                tasks: [
+                    { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+                    { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+                    { taskId: '3', title: "Write tests", status: "todo" },
+                ],
+                type: "Public"
+            },
+        ]
+    }
+]
---
+++ client/app/utils/data/projectData.ts
@@ -0,0 +1,64 @@
+import { ProjectDataType } from "../types/Types";
+
+export const projectData: ProjectDataType[] = [
+    {
+        projectId: '1',
+        name: "Project Alpha",
+        lead: "John Doe",
+        description: "This is a project description for Project Alpha. This is a project description for Project Alpha.",
+        tasks: [
+            { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+            { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+            { taskId: '3', title: "Write tests", status: "todo" },
+        ],
+        type: "Public"
+    },
+    {
+        projectId: '3',
+        name: "Project Alpha",
+        lead: "John Doe",
+        description: "This is a project description for Project Alpha. This is a project description for Project Alpha.",
+        tasks: [
+            { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+            { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+            { taskId: '3', title: "Write tests", status: "todo" },
+        ],
+        type: "Public"
+    },
+    {
+        projectId: '2',
+        name: "Project Alpha",
+        lead: "John Doe",
+        description: "This is a project description for Project Alpha. This is a project description for Project Alpha.",
+        tasks: [
+            { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+            { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+            { taskId: '3', title: "Write tests", status: "todo" },
+        ],
+        type: "Public"
+    },
+    {
+        projectId: '4',
+        name: "Project Alpha",
+        lead: "John Doe",
+        description: "This is a project description for Project Alpha. This is a project description for Project Alpha.",
+        tasks: [
+            { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+            { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+            { taskId: '3', title: "Write tests", status: "todo" },
+        ],
+        type: "Public"
+    },
+    {
+        projectId: '6',
+        name: "Project Alpha",
+        lead: "John Doe",
+        description: "This is a project description for Project Alpha. This is a project description for Project Alpha.",
+        tasks: [
+            { taskId: '1', title: "Design UI", status: "completed", assignee: "Jane Smith" },
+            { taskId: '2', title: "Implement backend", status: "in-progress", assignee: "John Doe" },
+            { taskId: '3', title: "Write tests", status: "todo" },
+        ],
+        type: "Public"
+    },
+]
