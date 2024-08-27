eb7f8d567b8b00cadc5de69e611f60d542333d2a
Danishaft <danielejeh2019@gmail.com>
2024-08-27 20:01:33 +0100

    update validation.ts

--- client/app/(auth)/validation.ts
+++ client/app/(auth)/validation.ts
@@ -2,12 +2,25 @@
 import { z } from "zod"
 import { Regex } from "../utils"
 
-export const defaultValues = {
+export const signInValues = {
     email: "",
 };
 
-export const authSchema = z.object({
+export const signInSchema = z.object({
   email: z.string().regex(Regex.isEmail, "please enter a valid email"),
 });
 
-export type IAuthSchema = z.infer<typeof authSchema>;
+export type ISignInSchema = z.infer<typeof signInSchema>;
+
+export const createOrgValues = {
+  firstName: "",
+  lastName: "",
+  workspaceName: "",
+}
+export const createOrgSchema = z.object({
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+  workspaceName: z.string().min(1)
+})
+
+export type ICreateOrgSchema = z.infer<typeof createOrgSchema>;
