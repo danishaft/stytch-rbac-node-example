2232961403aaa3be3362360bbcb9f3b85299a611
Danishaft <danielejeh2019@gmail.com>
2024-08-27 20:12:31 +0100

    update stytch.config.js

--- server/utils/stytch.config.js
+++ server/utils/stytch.config.js
@@ -1,1 +1,9 @@
 // Initializes the Stytch client with project credentials
+const stytch = require('stytch')
+
+const stytchClient = new stytch.B2BClient({
+    project_id: process.env.STYTCH_PROJECT_ID,
+    secret: process.env.STYTCH_SECRET,
+})
+
+module.exports = stytchClient
