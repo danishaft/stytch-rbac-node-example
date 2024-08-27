4fbfad7fd6bb73324409c246d72c1e07671efba5
Danishaft <danielejeh2019@gmail.com>
2024-08-27 20:05:02 +0100

    update layout.tsx

--- client/app/workspace/layout.tsx
+++ client/app/workspace/layout.tsx
@@ -3,19 +3,23 @@
 import { NavBar } from '@/components/layout/NavBar';
 import { SideBar } from '@/components/layout/sidebar/SideBar';
 import { useState } from 'react';
+import { useAuth } from '../utils';
 
 export default function WorkspaceLayout({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(false);
+  const {isAuthenticated, loading} = useAuth();
   return (
-    <div className='flex min-h-screen h-full'>
-      <SideBar isOpen={isOpen} />
-      <div className="flex-1 h-full md:ml-[15em]">
-        <NavBar isOpen={isOpen} setIsOpen={setIsOpen} />
-        <div className='p-4 h-full'>
-          {children}
+    isAuthenticated && (
+      <div className='flex min-h-screen h-full'>
+        <SideBar isOpen={isOpen} />
+        <div className="flex-1 h-full md:ml-[15em]">
+          <NavBar isOpen={isOpen} setIsOpen={setIsOpen} />
+          <div className='p-4 h-full'>
+            {children}
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
