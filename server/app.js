124b7fc1cce6bfd62e96ce94cea897e072652dc5
Danishaft <danielejeh2019@gmail.com>
2024-08-27 20:06:59 +0100

    update[server] app.js

--- server/app.js
+++ server/app.js
@@ -1,66 +1,78 @@
-const express = require('express'); 
-const createHttpError = require ('http-errors');
-require('dotenv/config');
-const passport = require('passport');
-const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const createHttpError = require('http-errors')
+const cors = require('cors')
+require('dotenv/config')
 const morgan = require('morgan')
-// const connectDB = require('./config/database');
-// const { passportConfig } = require('./config/passport-config');
-// const { stytchClient } = require('./config/stytch-config');
-// const { authenticateStytchSession } = require('./middleware/auth'); 
-
-// Local routes 
-const authRoute = require('./routes/auth.route');
-const orgRoutes = require('./routes/org.route');
-const projectRoutes = require('./routes/project.route');
-const tasksRoutes = require('./routes/tasks.route');
-const userRoutes = require('./routes/user.route');
 
 // initialize the server
-const app = express();
+const app = express()
+app.use(
+    cors({
+        origin: process.env.APP_URL, // Replace with your frontend URL
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        credentials: true,
+    })
+)
+app.options('*', cors())
 
-// Middlewares 
+// Middlewares
+app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+// app.use(passport.initialize());
+// Session management.
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: 'session-signing-secret',
+        cookie: { maxAge: 60000 },
+    })
+)
+app.use(bodyParser.json())
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
-// app.use(passport.initialize()); 
+// app.use((req, res, next) => {
+//     console.log('Incoming request:', req.method, req.path, req.headers);
+//     next();
+// });
+//middlewares
+// const authenticateStytchSessionJwt = require('./middleware/auth.middleware')
+
+// Local routes
+const authRoute = require('./routes/auth')
+const orgRoute = require('./routes/organization')
+const deptRoute = require('./routes/department')
 
 
 // Routes
 app.get('/', (req, res) => {
-	res.send('Hello from the root path!');
-});  
-app.use('/auth', authRoute); 
-// app.use('/org', orgRoutes); 
-// app.use('/projects', projectRoutes); 
-// app.use('/tasks', tasksRoutes); 
-// app.use('/user', userRoutes);
+    res.send('Hello from the root path!')
+})
+app.use('/api/auth', authRoute)
+app.use('/api/organizations', orgRoute)
+app.use('/api/organizations/departments', deptRoute)
 
 
 //error handlers
 app.use((req, res, next) => {
-    next(createHttpError.NotFound());
-});
+    next(createHttpError.NotFound())
+})
 app.use((err, req, res, next) => {
-    err.status = err.status || 500;
-	console.error(err.stack); 
-	const message = err.message || "Internal Server Error";
-    res.status(err.status).json({ error: message });
-});
+    err.status = err.status || 500
+    console.error(err.stack)
+    const message = err.message || 'Internal Server Error'
+    res.status(err.status).json({ error: message })
+})
 
-// Server Listen Along with Database 
-const PORT = process.env.PORT || 3000; 
-//connect mongoose and mongodb
-mongoose.connect(process.env.MONGODB_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-}).then(() => {
-	console.log('Connected to MongoDB');
-	app.listen(PORT, (err) => {
-		if (err) throw err;
-		console.log(`Server is Successfully Running, and App is listening on port ${PORT}`)
-	})
-}).catch((err) => console.log(err.message))
-
-
+// Server Listen Along with Database
+const PORT = process.env.PORT || 4000
+app.listen(PORT, (err) => {
+    if (err) throw err
+    console.log(
+        `Server is Successfully Running, and App is listening on port ${PORT}`
+    )
+})
