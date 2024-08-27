9c3a9812a8a98a5cce026c7739eb065302448c28
Danishaft <danielejeh2019@gmail.com>
2024-08-27 20:05:44 +0100

    feat: create spinner.tsx

---
+++ client/components/ui/spinner.tsx
@@ -0,0 +1,10 @@
+
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-16 h-16 border-4 border-newPrimary border-t-transparent border-solid rounded-full animate-spin"></div>
+    </div>
+  )
+}
+
+export default Spinner
