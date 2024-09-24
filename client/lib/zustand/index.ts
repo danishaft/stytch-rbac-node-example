60255d77482010d925cda74e7e83b1bea52a6eb5
Danishaft <danielejeh2019@gmail.com>
2024-09-24 20:26:50 +0100

    feat: create client/lib/zustand/index.ts

---
+++ client/lib/zustand/index.ts
@@ -0,0 +1,10 @@
+import { useDepartmentStore } from "./department";
+import { useOrgStore } from "./organization";
+import { useUserStore } from "./user";
+
+
+export const AppStores = {
+    useUserStore,
+    useOrgStore,
+    useDepartmentStore
+}
