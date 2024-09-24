f5cd31b455026dd61567bf8dc32147c7b0c303d0
Danishaft <danielejeh2019@gmail.com>
2024-09-24 20:28:45 +0100

    update UserProfile.tsx

--- client/components/layout/UserProfile.tsx
+++ client/components/layout/UserProfile.tsx
@@ -1,15 +1,21 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-export const UserProfile = () => {
+interface UserProfileProps {
+  name: string;
+  location: string;
+  image: string;
+}
+
+export const UserProfile: React.FC<UserProfileProps> = ({name, location, image}) => {
   return (
     <div className="flex items-center gap-1">
-        <div className="flex flex-col h-full">
-            <p className="text-[.8rem] font-medium">Ejeh Daniel</p>
-            <p className="text-[.55rem] mt-auto text-right">Abj, Nigeria</p>
+        <div className="flex flex-col h-full capitalize">
+            <p className="text-[.8rem] font-medium">{name}</p>
+            <p className="text-[.55rem] mt-auto text-right">{location}</p>
         </div>
         <div className="flex-shrink-0 ml-2">
             <Avatar className="h-10 w-10">
-                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarImage src={image} />
                 <AvatarFallback>CN</AvatarFallback>
             </Avatar>
         </div>   
