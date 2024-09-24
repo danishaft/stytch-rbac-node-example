73ecd3b1e39f6e49e585f84394864b2eaf1c3408
Danishaft <danielejeh2019@gmail.com>
2024-09-24 20:23:57 +0100

    update auth.middleware.js and passport.config.js

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -2,52 +2,74 @@
 // authenticateStytchSession: Verifies the Stytch session token
 // isAuthenticated: Checks if a user is authenticated
 // isOrganizationMember: Checks if a user belongs to an organization
-
+const passport = require('passport')
 const stytchClient = require('../utils/stytch.config')
 
-//middleware for authenticating a user
-const authenticateStytchSessionToken = async (req, res, next) => {
-        console.log('here')
-        const sessionToken = req.cookies.session_token
-        if (!sessionToken) {
-            return res.status(401).json({error: 'No session token provided'})
+//middleware for authenticating a user using passport
+const authenticateStytchSessionToken = (req, res, next) => {
+    passport.authenticate('stytch', { session: false }, (err, user, info) => {
+        console.log('in authenticateStytchSessionToken')
+        console.log(err)
+        if (err) {
+            return next(err)
         }
-        console.log(sessionToken)
-        try {
-            const response = await stytchClient.sessions.authenticate({  session_token: sessionToken })
-            const { session_token} = response
-            req.stytchSession = {session_token}
-            next()
-        } catch (error) {
-            console.error('Stytch API Error:', error)
-            if (error.response) {
-                console.error('Response:', error.response.data)
-            }
-            res.status(error.status_code || 500).json({
-                error: error.error_type || 'Invalid or expired session',
-                message:
-                    error.message || 'An error occurred during authentication',
-            })
+        if (!user) {
+            return res
+                .status(401)
+                .json({ error: info.message || 'Unauthorized' })
         }
-    
+        req.user = user
+        next()
+    })(req, res, next)
 }
+
+
+// const authenticateStytchSessionToken = async (req, res, next) => {
+//         console.log('here')
+//         const sessionToken = req.cookies.session_token
+//         if (!sessionToken) {
+//             return res.status(401).json({error: 'No session token provided'})
+//         }
+//         console.log(sessionToken)
+//         try {
+//             const response = await stytchClient.sessions.authenticate({  session_token: sessionToken })
+//             const { session_token} = response
+//             req.stytchSession = {session_token}
+//             next()
+//         } catch (error) {
+//             console.error('Stytch API Error:', error)
+//             if (error.response) {
+//                 console.error('Response:', error.response.data)
+//             }
+//             res.status(error.status_code || 500).json({
+//                 error: error.error_type || 'Invalid or expired session',
+//                 message:
+//                     error.message || 'An error occurred during authentication',
+//             })
+//         }
+    
+// }
+
+
+
 
 // middleware for ensuring the user requesting a route has permission to
 //  do the thing they’re trying to do. This uses Stytch RBAC authorization checks
 //  under the hood.
 const authenticateAndAuthorize = (resource, action) => {
     return async (req, res, next) => {
+        console.log('in authenticateAndAuthorize', req.user)
         const user = req.user
         if (!user) {
             return res.status(400).json('No user found')
         }
-        const sessionToken = req.cookies.session_token
-        console.log('available', sessionToken, user)
+        const sessionToken = req.cookies.stytch_session
+        // console.log('available', sessionToken, user)
         try {
             const response = await stytchClient.sessions.authenticate({
                 session_token: sessionToken,
                 authorization_check: {
-                    organization_id: user.organizationId,
+                    organization_id: user.member.organization_id,
                     resource_id: resource,
                     action,
                 },
@@ -63,6 +85,7 @@ const authenticateAndAuthorize = (resource, action) => {
                 throw new Error('Unauthorized')
             }
         } catch (error) {
+            console.log('eror', error.message)
             res.status(error.status_code || 401).json({
                 message: error.message || 'Unauthorized',
             })
--- server/utils/passport.config.js
+++ server/utils/passport.config.js
@@ -1,2 +1,34 @@
 // Configures Passport.js to use a custom Stytch strategy for authentication
+const passport = require('passport')
+const stytchClient = require('../utils/stytch.config')
+
+class StytchStrategy extends passport.Strategy {
+    constructor() {
+        super()
+        this.name = 'stytch'
+    }
+
+    async authenticate(req) {
+        const sessionToken = req.cookies.stytch_session
+        if (!sessionToken) {
+            return this.fail({ message: 'No session token provided' }, 401)
+        }
+        try {
+            const response = await stytchClient.sessions.authenticate({
+                session_token: sessionToken,
+            })
+            if (response.session_token) {
+                const { member, organization } = response
+                const user = { member, organization }
+                this.success(user)
+            } else {
+                this.fail({ message: 'Invalid session' }, 401)
+            }
+        } catch (error) {
+            this.error(error)
+        }
+    }
+}
+
+module.exports = StytchStrategy
 
