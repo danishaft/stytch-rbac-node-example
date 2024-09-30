c40ddae8f3ccecad76c4af024e73dfd779a35f41
Danishaft <danielejeh2019@gmail.com>
2024-09-30 05:35:40 +0100

    update client/lib/zustand/index.ts update

--- client/lib/zustand/index.ts
+++ client/lib/zustand/index.ts
@@ -1,10 +1,12 @@
 import { useDepartmentStore } from "./department";
 import { useOrgStore } from "./organization";
 import { useUserStore } from "./user";
+import {useProjectStore} from "./project"
 
 
 export const AppStores = {
     useUserStore,
     useOrgStore,
-    useDepartmentStore
+    useDepartmentStore,
+    useProjectStore
 }
